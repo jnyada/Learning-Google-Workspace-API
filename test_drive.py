d7d660bcaff6d8046917450acba7b8588a93e1fc
jorgeyada <88353578+jorgeyada@users.noreply.github.com>
2021-08-02 17:41:15 -0600

    Add files via upload
    This is for listing files from a given user

---
+++ test_drive.py
@@ -0,0 +1,25 @@
+from __future__ import print_function
+import os.path
+from googleapiclient.discovery import build
+from httplib2 import Http
+from google.oauth2 import credentials, service_account
+
+SCOPES = ["https://www.googleapis.com/auth/drive"]
+
+SERVICE_ACCOUNT_FILE = 'credentials.json'
+credentials = service_account.Credentials.from_service_account_file(SERVICE_ACCOUNT_FILE, scopes= SCOPES)
+delegated_creds = credentials.with_subject("melendezj@support-domain26.info")
+
+service = build('drive', 'v3', credentials = delegated_creds)
+
+results = service.files().list(orderBy = 'name', pageSize = 10, corpora= 'user').execute()
+items = results.get('files', [])
+
+if not items:
+    print("no files found")
+else:
+    print("files:")
+    for item in items:
+        print(u'{0} ({1})'.format(item['name'], item['id']))
+
+#print(results)
