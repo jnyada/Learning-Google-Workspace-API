cbb26df46a15b0641ef8d7bcee1a262fda50bbfb
jorgeyada <88353578+jorgeyada@users.noreply.github.com>
2021-08-03 17:07:49 -0600

    Update signatures_alias.py

--- signatures_alias.py
+++ signatures_alias.py
@@ -6,10 +6,11 @@ from google.oauth2 import service_account
 #creating the values to insert to theuser
 DATA = {'signature': "Test Signature"}
 
-SCOPES = ["https://www.googleapis.com/auth/gmail.settings.basic", "https://www.googleapis.com/auth/gmail.modify", "https://www.googleapis.com/auth/gmail.send", "https://mail.google.com" ]
+SCOPES=["https://www.googleapis.com/auth/gmail.settings.basic","https://www.googleapis.com/auth/gmail.modify","https://www.googleapis.com/auth/gmail.send","https://mail.google.com"]
 #declaring json file for service account
 SERVICE_ACCOUNT_FILE = 'credentials.json'
-#obtain credentials from the json from https://developers.google.com/identity/protocols/oauth2/service-account#authorizingrequests
+""" obtain credentials from the json file and pass them on to the delegated account 
+using impersonation as explained on https://developers.google.com/identity/protocols/oauth2/service-account#authorizingrequests """
 credentials = service_account.Credentials.from_service_account_file(SERVICE_ACCOUNT_FILE, scopes=SCOPES)
 delegated_credentials = credentials.with_subject("user@domain")
 #Making the call to the API
