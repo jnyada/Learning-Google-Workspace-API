00aba6249d33a7c5f27c13acb680416183fa4cda
jorgeyada <88353578+jorgeyada@users.noreply.github.com>
2021-11-03 15:38:35 -0600

    Update adding_sendMailAs.py

--- adding_sendMailAs.py
+++ adding_sendMailAs.py
@@ -3,9 +3,9 @@ from apiclient import discovery
 from google.oauth2 import service_account
 
 #creating the values to insert to theuser
-DATA = {'sendAsEmail': "testingapi@support-domain26.info",
+DATA = {'sendAsEmail': "alias@domain.com",
         "displayName":"Alias1",
-        "replyToAddress":"testingapi@support-domain26.info",
+        "replyToAddress":"alias@domain.com",
         "signature":"Alias1 test",
         "isPrimary": False,
         "treatAsAlias":True,
@@ -14,12 +14,12 @@ DATA = {'sendAsEmail': "testingapi@support-domain26.info",
 
 SCOPES = ["https://www.googleapis.com/auth/gmail.settings.basic", "https://www.googleapis.com/auth/gmail.settings.sharing", "https://www.googleapis.com/auth/gmail.modify",  "https://www.googleapis.com/auth/gmail.send", "https://mail.google.com" ]
 #declaring json file for service account
-SERVICE_ACCOUNT_FILE = 'adding-aliases.json'
+SERVICE_ACCOUNT_FILE = 'credentials.json' #path to credentials file 
 #obtain credentials from the json
 credentials = service_account.Credentials.from_service_account_file(SERVICE_ACCOUNT_FILE, scopes=SCOPES)
 #using the with_subject Method to pass on the credentials to the impersonated user
-delegated_credentials = credentials.with_subject("yadatest@support-domain26.info")
+delegated_credentials = credentials.with_subject("user@domain.com")
 #Making the call to the API
 GMAIL = discovery.build('gmail', 'v1', credentials = delegated_credentials)
 
-rsp = GMAIL.users().settings().sendAs().create(userId='yadatest@support-domain26.info', body=DATA).execute()
+rsp = GMAIL.users().settings().sendAs().create(userId='user@domain.com', body=DATA).execute()
