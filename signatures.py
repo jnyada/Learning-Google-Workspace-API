1dba4d0f7e3e1044327bed19bb363f0d8fee5bc4
jnyada <jorgeyada04@gmail.com>
2021-11-30 21:52:24 -0600

    I updated the signatures.py to be able to run a couple of functions I made, one to search for users and the other to apply a new signature to the users found with the first function. I update them as I am learning new concepts with python

--- signatures.py
+++ signatures.py
@@ -1,18 +1,42 @@
 from __future__ import print_function
 from apiclient import discovery
+from google.auth import credentials
 from google.oauth2 import service_account
+from oauth2client.client import SERVICE_ACCOUNT
 
-#creating the values to insert to theuser
-DATA = {'signature': '"Watching. Potential. Learn. Grow. Provoke. Consume. Reward. Patience." \n - Ukotoa"'}
+def targets():
+    targets = []
+    SCOPES = ['https://www.googleapis.com/auth/admin.directory.user', 'https://www.googleapis.com/auth/admin.directory.user.readonly', 'https://www.googleapis.com/auth/cloud-platform']
+    SERVICE_ACCOUNT_FILE= 'credentials.json'
+    credentials = service_account.Credentials.from_service_account_file(SERVICE_ACCOUNT_FILE, scopes=SCOPES)
+    creds = credentials.with_subject("admin@domain.com")
+    service = discovery.build('admin', 'directory_v1', credentials=creds)
 
-SCOPES = ["https://www.googleapis.com/auth/gmail.settings.basic", "https://www.googleapis.com/auth/gmail.modify", "https://www.googleapis.com/auth/gmail.send", "https://mail.google.com" ]
-#declaring json file for service account
-SERVICE_ACCOUNT_FILE = 'credentials.json' #this file is created from a project on google cloud platform.
-#obtain credentials from the json
-credentials = service_account.Credentials.from_service_account_file(SERVICE_ACCOUNT_FILE, scopes=SCOPES)
-#using the with_subject Method to pass on the credentials to the impersonated user
-delegated_credentials = credentials.with_subject("user@domain.com")
-#Making the call to the API
-GMAIL = discovery.build('gmail', 'v1', credentials = delegated_credentials)
+    results = service.users().list(customer='customer_id', maxResults=10, orderBy= 'email',query='String').execute()
+    #the query is optional
+    users = results.get('users', [])
+    for user in users:
+        targets.append(u'{0}'.format(user['primaryEmail']))
+    
+    return targets
 
-rsp = GMAIL.users().settings().sendAs().patch(userId='user@domain.com', sendAsEmail = 'user@domain.com', body=DATA).execute()
+def signature(target):
+    #creating the values to insert to theuser
+    DATA = {'signature': '"updating signatures for multiple userswith a couple of functions I made." \n - Yada"'}
+
+    SCOPES = ["https://www.googleapis.com/auth/gmail.settings.basic", "https://www.googleapis.com/auth/gmail.modify", "https://www.googleapis.com/auth/gmail.send", "https://mail.google.com" ]
+    #declaring json file for service account
+    SERVICE_ACCOUNT_FILE = 'credentialsb.json' #this file is created from a project on google cloud platform.
+    #obtain credentials from the json
+    credentials = service_account.Credentials.from_service_account_file(SERVICE_ACCOUNT_FILE, scopes=SCOPES)
+    #using the with_subject Method to pass on the credentials to the impersonated user
+    delegated_credentials = credentials.with_subject(target)
+    #Making the call to the API
+    GMAIL = discovery.build('gmail', 'v1', credentials = delegated_credentials)
+
+    rsp = GMAIL.users().settings().sendAs().patch(userId=target, sendAsEmail = target, body=DATA).execute()
+
+users = targets()
+
+for user in users:
+    signature(user)
